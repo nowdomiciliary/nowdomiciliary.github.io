0ac37076486d550076cca5e582542b9b18696bd4
nowdomiciliary <sgibbons625@gmail.com>
2015-09-19 21:51:08 -0400

    Move Repo from final_project

---
+++ sendEmail.php
@@ -0,0 +1,18 @@
+﻿<?php
+$action=$_REQUEST['action'];
+    {
+    $name=$_REQUEST['name'];
+    $email=$_REQUEST['email'];
+    $message=$_REQUEST['message'];
+    if (($name=="")||($email=="")||($message==""))
+        {
+        echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+        }
+    else{        
+        $from="From: $name<$email>\r\nReturn-path: $email";
+        $subject="Message sent using your contact form";
+        mail("steve@sgibbonsdesigns.com", $subject, $message, $from);
+        echo "Email sent!";
+        }
+    }  
+?>
